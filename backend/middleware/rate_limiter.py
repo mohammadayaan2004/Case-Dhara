4dcd15f0a1fd93c5d5f390e1bf91368c5da8e2da
Mohammad Ayaan Khan <149892891+mohammadayaan2004@users.noreply.github.com>
2026-05-15 19:11:23 +0530

    Delete backend directory

--- backend/middleware/rate_limiter.py
+++
@@ -1,8 +0,0 @@
-"""
-Global SlowAPI limiter for NyayaSetu (IP-based).
-"""
-
-from slowapi import Limiter
-from slowapi.util import get_remote_address
-
-limiter = Limiter(key_func=get_remote_address)
